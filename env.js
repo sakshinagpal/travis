52720205fca6867fdc673155f65dd9844d938d6b
sakshinagpal <sakshi.nagpal@comprotechnologies.com>
2016-09-12 18:05:12 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 console.log('@@@@@@@@@@@@@@@@@@@@@@@');
-console.log(process.env.TEST);
-var obj = JSON.parse(process.env.TEST);
-console.log(obj["book1"]);
-console.log(obj["book2"]);
+console.log(process.env.TEST1);
+//var obj = JSON.parse(process.env.TEST);
+//console.log(obj["book1"]);
+//console.log(obj["book2"]);
